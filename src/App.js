2adfa4ffa93d5672de1b4f3ee39a85d1b46709ba
Haischlong <hailong.dopham@outlook.de>
2023-03-07 23:09:31 +0100

    change color

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
 import './App.css';
+import React, { Component }  from 'react';
 
 function App() {
   return (
