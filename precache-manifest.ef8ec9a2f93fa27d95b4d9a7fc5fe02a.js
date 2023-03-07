bdcbc058176c3e2728c19f4d47b16dc61150f4f5
Haischlong <hailong.dopham@outlook.de>
2023-03-07 22:49:52 +0100

    Updates

---
+++ precache-manifest.ef8ec9a2f93fa27d95b4d9a7fc5fe02a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "71970cea759643cb36ac",
+    "url": "/Sorting-Visualizer/static/css/main.9611b4e2.chunk.css"
+  },
+  {
+    "revision": "71970cea759643cb36ac",
+    "url": "/Sorting-Visualizer/static/js/main.7991a33c.chunk.js"
+  },
+  {
+    "revision": "9e84e1da32c8f5f144f1",
+    "url": "/Sorting-Visualizer/static/js/runtime~main.91a755d8.js"
+  },
+  {
+    "revision": "fe79d1c3b3ae6eb4db12",
+    "url": "/Sorting-Visualizer/static/js/2.73313593.chunk.js"
+  },
+  {
+    "revision": "c563299317d52c1696cd",
+    "url": "/Sorting-Visualizer/static/js/3.937f6cc8.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Sorting-Visualizer/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c308ce6e0fa69c0b6f7005087226e8fc",
+    "url": "/Sorting-Visualizer/index.html"
+  }
+];
